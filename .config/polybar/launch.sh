69e83aeaea80c0c6273a55c9ebd37bc3b0b5a4df
Ashpex <38400042+Ashpex@users.noreply.github.com>
2020-01-19 15:45:24 +0700

    Add files via upload

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# set correct resolution
+#xrandr && xrandr --output eDP1 --mode 1920x1080
+
+# load background
+#feh --bg-fill ~/Media/Bilder/ArenaBlurArchFullHD.png
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar -r topbar &
